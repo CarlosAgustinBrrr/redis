8679aa70e8315d87e534c1d73dcfd317172e4eb5
Marcos Burgos <burgos.lomar18@zaragoza.salesianos.edu>
2024-02-25 18:27:35 +0100

    ajustes

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,21 +1,9 @@
 import express from "express";
-import { listarPersonas, agregarPersona, eliminarPersona, actualizarPersona, agregarJson, filtrarPersonas, listarOFiltrarPersonas } from "../controller/personas.js";
+import { agregarPersona, eliminarPersona, actualizarPersona, agregarJson, listarOFiltrarPersonas } from "../controller/personas.js";
 import multer from 'multer';
 
 const router = express.Router();
 const upload = multer({ dest: 'uploads/' })
-
-// router.get('/', async (req, res) => {
-//     try {
-//         //const personas = await obtenerPersonas();
-//         const personas = await listarPersonas();
-//         res.render('pages/index', { personas });
-
-//     } catch (error) {
-//         console.error('Error en la ruta :', error);
-//         res.redirect('/error');
-//     }
-// });
 
 router.get('/', listarOFiltrarPersonas);
 router.get('/add', (req, res) => {
@@ -28,6 +16,5 @@ router.post('/agregar', agregarPersona);
 router.post('/personas/eliminar/:dni', eliminarPersona);
 router.post('/personas/actualizar/:dni', actualizarPersona);
 router.post('/personas/cargar', upload.single('archivoPersonas'), agregarJson);
-router.get('/personas/filtrar', filtrarPersonas);
 
 export default router;
